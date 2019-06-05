91ddbfecdb467c44d61ec774b6f72216acdfeb8d
jupiterleaf <pikaq.snail@gmail.com>
2019-06-05 19:48:35 +1000

    first commit

--- WPFdemo/MainWindow.xaml.cs
+++ WPFdemo/MainWindow.xaml.cs
@@ -23,6 +23,22 @@ namespace WPFdemo
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            txt1.Text = "Button is Clicked";
+        }
+
+        private void StackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            txt2.Text = "Click event is bubbled to Stack Panel";
+        }
+
+        private void Window_Click(object sender, RoutedEventArgs e)
+        {
+            txt3.Text = "Click event is bubbled to Window";
         }
     }
 }
